511d8bb4d26e3e0b70309add43edc8190a4cf643
Ilya <illyavlasenko2004@gmail.com>
2023-02-18 00:38:43 +0200

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Will change tomorrow
+            Func<double,double,double> Add = (x,y) => x + y;
+            Func<double, double, double> Sub= (x, y) => x - y;
+            Func<double, double, double> Mul = (x, y) => x * y;
+            Func<double, double, double> Div = (x, y) =>
+            {
+                if (y != 0)
+                {
+                    return x / y;
+                }
+                else throw new Exception("'Y' can't be zero");
+            };
+
+            Console.WriteLine(Add(2, 56));
+            Console.WriteLine(Sub(2, 3));
+            Console.WriteLine(Mul(84,3));
+            Console.WriteLine(Div(34,4));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RandomNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumbers
+{
+    internal class Program
+    {
+        delegate int RandomNumber();
+
+
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
